a3c13cf33ed5bd89d84df11d91830ae4dcfebb1a
Artem Afonin <joniclife@gmail.com>
2019-12-30 12:04:32 +0700

    Add vue-resource

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
-import index from "pages/index.vue";
+import index from 'pages/index.vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
 
 const app = new Vue({
   render: h => h(index)
