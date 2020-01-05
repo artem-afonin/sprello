636fc332319cc59923d9f26dda75ece1268fe272
Artem Afonin <joniclife@gmail.com>
2020-01-05 23:51:24 +0700

    Fix href

--- frontend/globalDefines.js
+++ frontend/globalDefines.js
@@ -1,7 +1,7 @@
 module.exports = {
   navButtons: [
     {name: 'Главная', href: '/'},
-    {name: 'Доски', href: '/boards'},
+    {name: 'Доски', href: '/board'},
     {name: 'Пользователи', href: '/users'},
     {name: 'О Sprello', href: '/about'}
   ]
