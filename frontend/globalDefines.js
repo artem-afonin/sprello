74178cb3618364d972f3fc00e45a20c5a08b87e6
Artem Afonin <joniclife@gmail.com>
2020-01-05 23:42:36 +0700

    Create globalDefines module

---
+++ frontend/globalDefines.js
@@ -0,0 +1,8 @@
+module.exports = {
+  navButtons: [
+    {name: 'Главная', href: '/'},
+    {name: 'Доски', href: '/boards'},
+    {name: 'Пользователи', href: '/users'},
+    {name: 'О Sprello', href: '/about'}
+  ]
+}
