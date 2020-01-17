c87fae8b3111121ff6a1d02b425b9c4a32aab805
Vasiliy <cagefighter565@gmail.com>
2020-01-17 13:19:43 +0700

    Delete useless annotation

--- src/main/java/ru/sprello/model/Task.java
+++ src/main/java/ru/sprello/model/Task.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="id")
     private Long id;
     @Column(name = "name")
     private String name;
