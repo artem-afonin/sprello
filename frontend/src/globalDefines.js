4e1d32d327e65bb5c7d0a8c9749d2e1686ac4d43
Artem Afonin <joniclife@gmail.com>
2020-01-26 19:09:28 +0700

    Refactor globalDefines apiurl

--- frontend/src/globalDefines.js
+++ frontend/src/globalDefines.js
@@ -1,10 +1,17 @@
 module.exports = {
   devMode: process.env.NODE_ENV === 'development',
-  apiurl: 'api/v1',
   navButtons: [
     {name: 'Главная', href: '/', authRequired: false},
     {name: 'Доски', href: '/board', authRequired: true},
     {name: 'Пользователи', href: '/users', authRequired: true},
     {name: 'О Sprello', href: '/about', authRequired: false}
-  ]
+  ],
+  apiurl: {
+    board: 'api/v1/board',
+    task: 'api/v1/board/task',
+    taskElement: 'api/v1/board/task/element',
+    user: 'api/v1/user',
+    boardId: id => `api/v1/board/${id}`,
+    userId: id => `api/v1/user/${id}`
+  }
 }
